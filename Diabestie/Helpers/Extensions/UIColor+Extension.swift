84c445e7a1f38537b7177d4e05745646f11ba7d2
Dhiky Aldwiansyah <dhikyaldwiansyah@gmail.com>
2021-03-31 21:58:51 +0700

    Update assets

--- Diabestie/Helpers/Extensions/UIColor+Extension.swift
+++ Diabestie/Helpers/Extensions/UIColor+Extension.swift
@@ -14,6 +14,19 @@ extension UIColor {
     static let accent = UIColor.color(named: "Accent")
     static let primary = UIColor.color(named: "Primary")
     static let secondary = UIColor.color(named: "Secondary")
+    static let black = UIColor.color(named: "black")
+    static let blue = UIColor.color(named: "blue")
+    static let grayText = UIColor.color(named: "gray_text")
+    static let greenBackground = UIColor.color(named: "green_background")
+    static let greenFood = UIColor.color(named: "green_food")
+    static let greenText = UIColor.color(named: "green_text")
+    static let orangeBackground = UIColor.color(named: "orange_background")
+    static let orangeText = UIColor.color(named: "orange_text")
+    static let purpleMedicine = UIColor.color(named: "purple_medicine")
+    static let purpleText = UIColor.color(named: "purple_text")
+    static let redBackground = UIColor.color(named: "red_background")
+    static let redBlood = UIColor.color(named: "red_blood")
+    static let redText = UIColor.color(named: "red_text")
 
     private static func color(named: String) -> UIColor {
         return UIColor(named: named)!
--- Diabestie/Helpers/Extensions/UIImage+Extension.swift
+++ Diabestie/Helpers/Extensions/UIImage+Extension.swift
@@ -12,7 +12,14 @@ import UIKit
 
 extension UIImage {
 
-    static let blood = UIImage.image(named: "blood")
+    static let icBlood = UIImage.image(named: "ic_blood")
+    static let icCheck = UIImage.image(named: "ic_check")
+    static let icDate = UIImage.image(named: "ic_date")
+    static let icDown = UIImage.image(named: "ic_down")
+    static let icFood = UIImage.image(named: "ic_food")
+    static let icMedicine = UIImage.image(named: "ic_medicine")
+    static let icUp = UIImage.image(named: "ic_up")
+    static let placeholderPerson = UIImage.image(named: "placeholder_person")
 
     private static func image(named: String) -> UIImage {
         return UIImage(named: named)!
