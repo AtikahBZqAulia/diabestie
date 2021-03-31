b7caa4cd82839e2a104fedf32eaa17cd2d39b38e
Dhiky Aldwiansyah <dhikyaldwiansyah@gmail.com>
2021-03-31 11:39:41 +0700

    Update

--- Diabestie/Presentation/DiaryViewController.swift
+++ Diabestie/Presentation/DiaryViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import AAInfographics
 
 
 class DiaryViewController: UIViewController {
     
+   private var aaChartView: AAChartView!
+    
+    @IBOutlet weak var viewChart: UIView!
+    private var aaChartModel: AAChartModel!
+    public var chartType: AAChartType!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        setUpAAChartView()
+
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -24,6 +32,68 @@ class DiaryViewController: UIViewController {
         }
     }
     
+    
+    
+    func setUpAAChartView(){
+        
+        aaChartView = AAChartView()
+        chartType = .column
+        
+        let chartViewWidth = view.frame.size.width
+        let chartViewHeight = view.frame.size.height - 220
+        aaChartView!.frame = viewChart.frame
+        view.addSubview(aaChartView!)
+        aaChartView!.scrollEnabled = false//Disable chart content scrolling
+        aaChartView!.isClearBackgroundColor = true
+        aaChartView!.delegate = self as AAChartViewDelegate
+        
+//        let aaOptions = AAOptions.init()
+//        aaOptions.tooltip?
+//                        .shadow(false)
+//                        .positioner("""
+//                        function (labelWidth, labelHeight, point) {
+//                            return {
+//                             x : point.plotX,
+//                             y : 0
+//                            };
+//                        }
+//                        """)
+//
+        
+        aaChartModel = AAChartModel()
+            .chartType(chartType!)
+            .colorsTheme(["#1e90ff","#ef476f","#ffd066","#04d69f","#25547c",])//Colors theme
+            .axesTextColor(AAColor.white)
+            .dataLabelsEnabled(false)
+            .tooltipValueSuffix("℃")
+            .animationType(.bounce)
+            .touchEventEnabled(true)
+            .series([
+                AASeriesElement()
+                    .name("Tokyo")
+                    .data([0,7.0, 6.9, 9.5])
+                ])
+    
+        configureColumnChartAndBarChartStyle()
+        
+//        aaChartView!.aa_drawChartWithChartOptions(aaOptions)
+        let aaOptions = AAOptionsConstructor.configureChartOptions(aaChartModel)
+        aaOptions.yAxis?.opposite(true)
+
+        aaChartView!.aa_drawChartWithChartModel(aaChartModel!)
+        aaChartView!.aa_drawChartWithChartOptions(aaOptions)
+        viewChart.addSubview(aaChartView)
+    }
+    
+    private func configureColumnChartAndBarChartStyle() {
+        aaChartModel!
+            .categories(["0", "12", "18", "23"])
+            .legendEnabled(false)
+            .colorsTheme(["#fe117c","#ffc069","#06caf4","#7dffc0"])
+            .animationType(.easeOutCubic)
+            .animationDuration(1200)
+        
+    }
 }
 
 extension DiaryViewController : TableViewControllerDelegate {
@@ -32,4 +102,34 @@ extension DiaryViewController : TableViewControllerDelegate {
         print("DO SOMETING WITH IT \(data)")
     }
     
+}
+
+
+extension DiaryViewController: AAChartViewDelegate {
+    open func aaChartViewDidFinishLoad(_ aaChartView: AAChartView) {
+       print("🙂🙂🙂, AAChartView Did Finished Load!!!")
+    }
+
+    open func aaChartView(_ aaChartView: AAChartView, moveOverEventMessage: AAMoveOverEventMessageModel) {
+        print(
+            """
+
+            selected point series element name: \(moveOverEventMessage.name ?? "")
+            🔥🔥🔥WARNING!!!!!!!!!!!!!!!!!!!! Touch Event Message !!!!!!!!!!!!!!!!!!!! WARNING🔥🔥🔥
+            ==========================================================================================
+            ------------------------------------------------------------------------------------------
+            user finger moved over!!!,get the move over event message: {
+            category = \(String(describing: moveOverEventMessage.category))
+            index = \(String(describing: moveOverEventMessage.index))
+            name = \(String(describing: moveOverEventMessage.name))
+            offset = \(String(describing: moveOverEventMessage.offset))
+            x = \(String(describing: moveOverEventMessage.x))
+            y = \(String(describing: moveOverEventMessage.y))
+            }
+            +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+            
+            
+            """
+        )
+    }
 }
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Diabestie' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Diabestie
+  pod 'AAInfographics', :git => 'https://github.com/AAChartModel/AAChartKit-Swift.git'
+
+end
