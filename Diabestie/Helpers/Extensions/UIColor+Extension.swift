b31ffe3ce80ed555b9f421574f28a5785663dc1a
Dhiky Aldwiansyah <dhikyaldwiansyah@gmail.com>
2021-04-01 13:09:25 +0700

    Generate assets file from swiftgen

--- Diabestie/Helpers/Extensions/UIColor+Extension.swift
+++ Diabestie/Helpers/Extensions/UIColor+Extension.swift
@@ -11,22 +11,22 @@ import UIKit
 
 extension UIColor {
 
-    static let accent = UIColor.color(named: "Accent")
-    static let primary = UIColor.color(named: "Primary")
-    static let secondary = UIColor.color(named: "Secondary")
     static let black = UIColor.color(named: "black")
-    static let blue = UIColor.color(named: "blue")
-    static let grayText = UIColor.color(named: "gray_text")
+    static let blueBlue = UIColor.color(named: "blue_blue")
+    static let blueGreen = UIColor.color(named: "blue_green")
+    static let bluePurple = UIColor.color(named: "blue_purple")
+    static let charcoalGrey = UIColor.color(named: "charcoal_grey")
+    static let grayBackground = UIColor.color(named: "gray_background")
     static let greenBackground = UIColor.color(named: "green_background")
-    static let greenFood = UIColor.color(named: "green_food")
-    static let greenText = UIColor.color(named: "green_text")
-    static let orangeBackground = UIColor.color(named: "orange_background")
-    static let orangeText = UIColor.color(named: "orange_text")
+    static let greenLight = UIColor.color(named: "green_light")
+    static let lightPeriwinkle = UIColor.color(named: "light_periwinkle")
+    static let paleGrey = UIColor.color(named: "pale_grey")
     static let purpleMedicine = UIColor.color(named: "purple_medicine")
-    static let purpleText = UIColor.color(named: "purple_text")
     static let redBackground = UIColor.color(named: "red_background")
-    static let redBlood = UIColor.color(named: "red_blood")
-    static let redText = UIColor.color(named: "red_text")
+    static let redOrange = UIColor.color(named: "red_orange")
+    static let reddishPink = UIColor.color(named: "reddish_pink")
+    static let tangerine = UIColor.color(named: "tangerine")
+    static let tangerineBackground = UIColor.color(named: "tangerine_background")
 
     private static func color(named: String) -> UIColor {
         return UIColor(named: named)!
--- Diabestie/Helpers/Extensions/UIImage+Extension.swift
+++ Diabestie/Helpers/Extensions/UIImage+Extension.swift
@@ -12,13 +12,7 @@ import UIKit
 
 extension UIImage {
 
-    static let icBlood = UIImage.image(named: "ic_blood")
-    static let icCheck = UIImage.image(named: "ic_check")
-    static let icDate = UIImage.image(named: "ic_date")
-    static let icDown = UIImage.image(named: "ic_down")
-    static let icFood = UIImage.image(named: "ic_food")
-    static let icMedicine = UIImage.image(named: "ic_medicine")
-    static let icUp = UIImage.image(named: "ic_up")
+    static let food = UIImage.image(named: "food")
     static let placeholderPerson = UIImage.image(named: "placeholder_person")
 
     private static func image(named: String) -> UIImage {
