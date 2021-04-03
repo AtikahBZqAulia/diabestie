c9a7dff06a06501cd2deadb5eaeb9ec206373dd0
revarinoo <revarinoputra@gmail.com>
2021-04-04 00:13:05 +0700

    Food Intake Detail

---
+++ Diabestie/Presentation/Food Intake Detail/FoodIntakeDetailViewController.swift
@@ -0,0 +1,70 @@
+//
+//  FoodIntakeDetailViewController.swift
+//  Diabestie
+//
+//  Created by Revarino Putra on 03/04/21.
+//
+
+import UIKit
+
+class FoodIntakeDetailViewController: UIViewController {
+    
+    
+    @IBOutlet weak var foodDetailTableView: UITableView!
+    
+    let food = ["","","Pisang", "Nasi", "Marugame", "Boba" , "Kol Goreng"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        foodDetailTableView.dataSource = self
+        foodDetailTableView.register(UINib(nibName: "FoodIntakeDetailTableCell", bundle: nil), forCellReuseIdentifier: "foodDetailCell")
+    }
+
+}
+
+extension FoodIntakeDetailViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return food.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        if indexPath.row > 1 {
+            if let cell = foodDetailTableView.dequeueReusableCell(withIdentifier: "foodDetailCell", for: indexPath) as? FoodIntakeDetailTableCell {
+
+                cell.foodName.text = food[indexPath.row]
+                cell.foodGram.text = "100 g"
+                cell.foodCal.text = "125 kcal"
+                cell.foodSugar.text = "146 mg sugar"
+                return cell
+            }
+            else {
+                return UITableViewCell()
+            }
+
+        }
+        
+        else if indexPath.row == 0 {
+            guard let cell = foodDetailTableView.dequeueReusableCell(withIdentifier: "calculatedCell") else {
+                return UITableViewCell()
+            }
+            hideSeparator(cell)
+            return cell
+        }
+        
+        else if indexPath.row == 1 {
+            guard let cell = foodDetailTableView.dequeueReusableCell(withIdentifier: "labelCell") else {
+                return UITableViewCell()
+            }
+            hideSeparator(cell)
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
+    
+    private func hideSeparator(_ cell: UITableViewCell) -> Void {
+        cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: .greatestFiniteMagnitude)
+    }
+}
---
+++ Diabestie/Presentation/Food Intake Detail/Table Cell/FoodIntakeDetailTableCell.swift
@@ -0,0 +1,28 @@
+//
+//  FoodIntakeDetailTableCell.swift
+//  Diabestie
+//
+//  Created by Revarino Putra on 03/04/21.
+//
+
+import UIKit
+
+class FoodIntakeDetailTableCell: UITableViewCell {
+
+    @IBOutlet weak var foodName: UILabel!
+    @IBOutlet weak var foodGram: UILabel!
+    @IBOutlet weak var foodCal: UILabel!
+    @IBOutlet weak var foodSugar: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
