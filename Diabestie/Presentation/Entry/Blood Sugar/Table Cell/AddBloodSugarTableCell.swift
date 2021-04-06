cd5871db392327fd38461a012e2cb122c992d7e4
imwurie <wuriandita.7@gmail.com>
2021-04-06 22:20:54 +0700

    Add AddBloodSugarTableCell.swift

--- Diabestie/Presentation/Entry/Blood Sugar/Table Cell/AddBloodSugarTableCell.swift
+++ Diabestie/Presentation/Entry/Blood Sugar/Table Cell/AddBloodSugarTableCell.swift
@@ -5,4 +5,19 @@
 //  Created by Wuri Dita on 06/04/21.
 //
 
-import Foundation
+import UIKit
+
+class AddBloodSugarTableCell: UITableViewCell {
+    
+    static let identifier = "AddBloodSugarTableCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    
+}
