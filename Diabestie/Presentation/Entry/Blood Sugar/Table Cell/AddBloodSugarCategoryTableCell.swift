d696693cdb890652ba4d84e87a7344de49ea3b73
imwurie <wuriandita.7@gmail.com>
2021-04-06 22:56:26 +0700

    Edit typo in identifier variable

--- Diabestie/Presentation/Entry/Blood Sugar/Table Cell/AddBloodSugarCategoryTableCell.swift
+++ Diabestie/Presentation/Entry/Blood Sugar/Table Cell/AddBloodSugarCategoryTableCell.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class AddBloodSugarCategoryTableCell: UITableViewCell {
     
-    static let identifier = "AddBloodSugarTableCell"
+    static let identifier = "AddBloodSugarCategoryTableCell"
     
     override func awakeFromNib() {
         super.awakeFromNib()
