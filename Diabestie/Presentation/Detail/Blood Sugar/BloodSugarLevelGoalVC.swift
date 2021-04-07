a4bfa254f4e46e22ede93b158eea90cb0f51ff51
Dhiky Aldwiansyah <dhikyaldwiansyah@gmail.com>
2021-04-07 21:49:41 +0700

    Add blood sugar level goals

--- Diabestie/Presentation/Detail/Blood Sugar/BloodSugarLevelGoalVC.swift
+++ Diabestie/Presentation/Detail/Blood Sugar/BloodSugarLevelGoalVC.swift
@@ -7,14 +7,31 @@
 
 import UIKit
 
-class BloodSugarLevelGoalVC: UIViewController {
+class BloodSugarLevelGoalVC: UITableViewController {
 
+    @IBOutlet weak var edtFastingUpperBound: UITextField!
+    @IBOutlet weak var edtFastingLowerBound: UITextField!
+    @IBOutlet weak var edtMealUpperBound: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+
+        let header : UITableViewHeaderFooterView = view as! UITableViewHeaderFooterView
+    
+        header.textLabel?.font = .systemFont(ofSize: 14, weight: .regular)
+        header.textLabel?.textColor = .charcoalGrey
+        header.textLabel?.text =  header.textLabel?.text
+
+
+        header.backgroundView =  UIView(frame: header.bounds)
+        header.backgroundView?.backgroundColor = .grayBackground
+
+    }
 
     /*
     // MARK: - Navigation
