09af4676afbc540bae76faff679a5d106e9c0c7e
Dhiky Aldwiansyah <dhikyaldwiansyah@gmail.com>
2021-04-07 22:00:12 +0700

    Fix section label blood sugal level input

--- Diabestie/Presentation/Detail/Blood Sugar/BloodSugarLevelGoalVC.swift
+++ Diabestie/Presentation/Detail/Blood Sugar/BloodSugarLevelGoalVC.swift
@@ -21,18 +21,32 @@ class BloodSugarLevelGoalVC: UITableViewController {
     
     override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
 
-        let header : UITableViewHeaderFooterView = view as! UITableViewHeaderFooterView
-    
-        header.textLabel?.font = .systemFont(ofSize: 14, weight: .regular)
-        header.textLabel?.textColor = .charcoalGrey
-        header.textLabel?.text =  header.textLabel?.text
-
-
-        header.backgroundView =  UIView(frame: header.bounds)
-        header.backgroundView?.backgroundColor = .grayBackground
+//        let header : UITableViewHeaderFooterView = view as! UITableViewHeaderFooterView
+//
+//        header.textLabel?.font = .systemFont(ofSize: 14, weight: .regular)
+//        header.textLabel?.textColor = .charcoalGrey
+//        header.textLabel?.text =  header.textLabel?.text
+//
+//
+//        header.backgroundView =  UIView(frame: header.bounds)
+//        header.backgroundView?.backgroundColor = .grayBackground
 
     }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerFrame = tableView.frame
 
+        let title = UILabel()
+        title.frame =  CGRect(x: 16, y: 20, width: headerFrame.size.width-20, height: 20) //width equals to parent view with 10 left and right margin
+        title.font = title.font.withSize(14)
+        title.text = self.tableView(tableView, titleForHeaderInSection: section) //This will take title of section from 'titleForHeaderInSection' method or you can write directly
+        title.textColor = .charcoalGrey
+
+        let headerView:UIView = UIView(frame: CGRect(x: 0, y: 0, width: headerFrame.size.width, height: headerFrame.size.height))
+        headerView.addSubview(title)
+
+        return headerView
+    }
     /*
     // MARK: - Navigation
 
