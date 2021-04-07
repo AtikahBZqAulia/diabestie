e831ac5f9be33afdff5b8cdf12ae602444684046
juliuscesario <75230036+juliuscesario@users.noreply.github.com>
2021-04-07 18:23:14 +0800

    Add Cancel Button & fix color on Entry
    Calendar fix now and fix color RGB on Cancel button Entry

--- Diabestie/Presentation/Home & History/History/CalendarController.swift
+++ Diabestie/Presentation/Home & History/History/CalendarController.swift
@@ -10,11 +10,16 @@ import UIKit
 class CalendarController: UIViewController {
     
     @IBOutlet weak var CalendarPicker: UIDatePicker!
+    @IBOutlet weak var CancelAction: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Select Date"
         CalendarPicker.preferredDatePickerStyle = .inline
+    }
+    @IBAction func CancelPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
     
