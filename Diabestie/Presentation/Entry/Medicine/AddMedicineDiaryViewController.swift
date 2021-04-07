9fcc5ce96c5e7f13fa03748b4a8c15a73b62cb5a
revarinoo <revarinoputra@gmail.com>
2021-04-07 11:47:58 +0700

    Update Add Medicine Diary (Stepper)

--- Diabestie/Presentation/Entry/Medicine/AddMedicineDiaryViewController.swift
+++ Diabestie/Presentation/Entry/Medicine/AddMedicineDiaryViewController.swift
@@ -8,22 +8,50 @@
 import UIKit
 
 class AddMedicineDiaryViewController: UIViewController {
-
+    
+    @IBOutlet weak var addMedicineTableView: UITableView!
+    
+    private let names = ["Fortamet", "Glumetza", "Replaginide"]
+    private let times = [1 , 3 , 2]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        addMedicineTableView.dataSource = self
+//        self.navigationController?.navigationBar.topItem?.title = ""
+    }
 
-        // Do any additional setup after loading the view.
+}
+
+extension AddMedicineDiaryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count + 1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            let cell = addMedicineTableView.dequeueReusableCell(withIdentifier: "createMedicine", for: indexPath)
+            return cell
+        }
+        else {
+            if let cell = addMedicineTableView.dequeueReusableCell(withIdentifier: "medicineList", for: indexPath) as? AddMedicineTableCell {
+                let select: Int = indexPath.row - 1
+                
+                var stringTimes: String!
+                if times[select] == 1 {
+                    stringTimes = "\(times[select]) time a day"
+                }
+                else {
+                    stringTimes = "\(times[select]) times a day"
+                }
+                cell.medicineName.text = names[select]
+                cell.medicineTimes.text = stringTimes
+                return cell
+            }
+            else {
+                return UITableViewCell()
+            }
+        }
+        
+        
     }
-    */
-
 }
