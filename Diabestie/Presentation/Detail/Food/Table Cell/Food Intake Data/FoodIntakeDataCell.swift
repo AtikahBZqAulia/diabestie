469a2d390b5586ceaf18ed1c3e88070a1d97b9f0
Dhiky Aldwiansyah <dhikyaldwiansyah@gmail.com>
2021-04-07 14:15:34 +0700

    Add food detail intake data scene

---
+++ Diabestie/Presentation/Detail/Food/Table Cell/Food Intake Data/FoodIntakeDataCell.swift
@@ -0,0 +1,23 @@
+//
+//  FoodIntakeDataCell.swift
+//  Diabestie
+//
+//  Created by Dhiky Aldwiansyah on 07/04/21.
+//
+
+import UIKit
+
+class FoodIntakeDataCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
