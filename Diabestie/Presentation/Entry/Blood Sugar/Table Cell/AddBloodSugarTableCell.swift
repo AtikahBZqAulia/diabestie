235dce6c8b612ae2e00ccf83947ca1c3aae9658e
imwurie <wuriandita.7@gmail.com>
2021-04-08 11:33:36 +0700

    Add text field input

--- Diabestie/Presentation/Entry/Blood Sugar/Table Cell/AddBloodSugarTableCell.swift
+++ Diabestie/Presentation/Entry/Blood Sugar/Table Cell/AddBloodSugarTableCell.swift
@@ -11,13 +11,25 @@ class AddBloodSugarTableCell: UITableViewCell {
     
     static let identifier = "AddBloodSugarTableCell"
     
+    @IBOutlet weak var textFieldBloodSugar: UITextField!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        textFieldBloodSugar.placeholder = "Add"
+        textFieldBloodSugar.delegate = self
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
+
+}
+
+extension AddBloodSugarTableCell: UITextFieldDelegate {
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        textFieldBloodSugar.resignFirstResponder()
+    }
     
 }
