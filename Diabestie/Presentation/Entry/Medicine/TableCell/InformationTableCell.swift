e4e4937fa9e2825b99d76df5127a5bb39ffd490d
revarinoo <revarinoputra@gmail.com>
2021-04-09 10:50:29 +0700

    Fix error

--- Diabestie/Presentation/Entry/Medicine/TableCell/InformationTableCell.swift
+++ Diabestie/Presentation/Entry/Medicine/TableCell/InformationTableCell.swift
@@ -24,12 +24,6 @@ class InformationTableCell: UITableViewCell {
         dismissPickerView()
     }
     
-    
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
