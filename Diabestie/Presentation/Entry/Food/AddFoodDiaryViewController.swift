8d3efcdb75b5aad750b580967305b506eef8f4fc
AtikahBZqAulia <atikahap.yes@gmail.com>
2021-04-14 11:07:12 +0700

    Add calculation for total calories and sugar

--- Diabestie/Presentation/Entry/Food/AddFoodDiaryViewController.swift
+++ Diabestie/Presentation/Entry/Food/AddFoodDiaryViewController.swift
@@ -31,6 +31,7 @@ class AddFoodDiaryViewController: UIViewController {
     @IBOutlet weak var sugarTotalLabel: UILabel!
     @IBOutlet weak var sugarMgLabel: UILabel!
     
+    
     var foodList: [FoodLibraries] = []
     var foodBaskets: [FoodBasket] = []
     
@@ -76,6 +77,9 @@ class AddFoodDiaryViewController: UIViewController {
     
     @IBAction func undwindFoodSegue(_ sender: UIStoryboardSegue){
         validateData()
+        let nutriton = FoodBasketRepository.shared.getFoodEntryTotalNutrition(entry: foodBaskets)
+        caloriesTotalLabel.text = "\(nutriton.calorie)"
+        sugarTotalLabel.text = "\(nutriton.sugar)"
         self.foodEntryTableView.reloadData()
     }
     
@@ -97,6 +101,9 @@ class AddFoodDiaryViewController: UIViewController {
             let vc = segue.destination as! SearchFoodViewController
             vc.baskets = foodBaskets
             vc.timeLog = timeLog
+            let nutriton = FoodBasketRepository.shared.getFoodEntryTotalNutrition(entry: foodBaskets)
+            caloriesTotalLabel.text = "\(nutriton.calorie)"
+            sugarTotalLabel.text = "\(nutriton.sugar)"
         }
     }
     
@@ -115,6 +122,9 @@ extension AddFoodDiaryViewController: FoodEntryDelegate {
                 basket.qty = Int32(newValue)
             }
         }
+        let nutriton = FoodBasketRepository.shared.getFoodEntryTotalNutrition(entry: foodBaskets)
+        caloriesTotalLabel.text = "\(nutriton.calorie)"
+        sugarTotalLabel.text = "\(nutriton.sugar)"
     }
     
     func onCategoryPick(categoryId: Int) {
@@ -157,10 +167,8 @@ extension AddFoodDiaryViewController: UITableViewDataSource{
         if let data = foodEntry {
             
             let nutriton = FoodEntryRepository.shared.getFoodEntryTotalNutrition(entry: data)
-            print("\(nutriton.sugar)")
-            print("\(nutriton.calorie)")
-            lblSugar.text = "\(nutriton.sugar)"
-            lblCalories.text = "\(nutriton.calorie)"
+            caloriesTotalLabel.text = "\(nutriton.calorie)"
+            sugarTotalLabel.text = "\(nutriton.sugar)"
         }
     }
     
